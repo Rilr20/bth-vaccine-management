a2af98ce9506d31143eac53635db3e61b61b65cb
Rilr20 <nrrl99@gmail.com>
2021-09-24 23:13:42 +0200

    creates admin user when migrating

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -17,11 +19,15 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
+        $user = new User();
+        $user->name = "admin";
+        $user->email = "admin";
+        $user->password = Hash::make("password");
+        $user->save();
+        
     }
 
     /**
