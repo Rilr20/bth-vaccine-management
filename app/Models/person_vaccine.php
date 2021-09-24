593e803fd49b804ac6d1d8482834f6dba4848fe6
Rilr20 <nrrl99@gmail.com>
2021-09-24 23:21:24 +0200

    person_vaccine table and model

---
+++ app/Models/person_vaccine.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class person_vaccine extends Model
+{
+    use HasFactory;
+
+    protected $dateFormat  = 'Y/m/d H:i:s';
+
+    protected $timestamp = false;
+    /*
+    CREATE TABLE person_vaccine (
+        id INT AUTO_INCREMENT NOT NULL,
+        person_id INT NOT NULL,
+        staff_id INT NOT NULL,
+        vaccine_id INT NOT NULL,
+        date_taken DATETIME NOT NULL,
+        expiration_date DATE NOT NULL,
+        
+        PRIMARY KEY (id),
+        FOREIGN KEY (person_id) REFERENCES patient(personnumber),
+        FOREIGN KEY (vaccine_id) REFERENCES vaccine(vaccine_id),
+        FOREIGN KEY (staff_id) REFERENCES staff(id)
+    );
+    */
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'person_id',
+        'staff_id',
+        'vaccine_id',
+        'date_taken',
+        'expiration_date',
+    ];
+
+}
---
+++ database/migrations/2021_09_24_114205_create_person_vaccines_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonVaccinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person_vaccines', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('patient_id');//->references('personnumber')->on('patients');//foreign key
+            $table->unsignedBigInteger('staff');//->references('id')->on('users'); //foreign key
+            $table->unsignedBigInteger('vaccine_id');//->references('id')->on('vaccines');//foreign key
+            $table->dateTime('date_taken');
+            $table->date('expiration_date');
+            // $table->timestamps();
+            // $table->f oreignId('users_idj')->constrained();
+            // $table->foreignId('staff')
+            // $table->foreignId('patient')
+            // $table->foreignId('vaccine')
+            // $table->foreign('patient')->references('personnumber')->on('patients');
+
+            $table->foreign('staff')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('vaccine_id')->references('id')->on('vaccines')->onDelete('cascade');
+            $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('person_vaccines');
+    }
+}
